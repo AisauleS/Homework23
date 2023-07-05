6e039e54f7035c890f057b9086b5ecc961fc13a5
AisauleS <aisaulesibatova@gmail.com>
2023-07-05 11:58:15 +0500

    Initial Commit

---
+++ Homework23/Homework23App.swift
@@ -0,0 +1,17 @@
+//
+//  Homework23App.swift
+//  Homework23
+//
+//  Created by Aisaule Sibatova on 05.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Homework23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
